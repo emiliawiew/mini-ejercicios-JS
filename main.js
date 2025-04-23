7a20b9fb9af97b861fbd269d98d512183a10ffd4
emiliawiew <wiewiora.emilia@gmail.com>
2025-04-23 17:15:18 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,65 +1,11 @@
-/* elcommentarion puede ser asi */
-// o puede ser asi con dos barras
+let nombre = (prompt("Cual es tu nombre?"));
+let ciudad = (prompt("Donde vives?"));
+let hobby = (prompt("Cual es tu hobby?"));
+document.writeln (" Bienvenido " + nombre + " ahora sé que vives en " + ciudad + " y te gusta " + hobby)
+console.log (" Bienvenido " + nombre + " ahora sé que vives en " + ciudad + " y te gusta " + hobby)
 
-//variables: nombre de variable y valor de la variable
-
-let x = 100;
-let y = 200;
-let z = parseInt(prompt("tenemos los valores x,y. Introduzca el valor z"));
-
-console.log("la suma de los tres numeros es:");
-console.log(x+y+z);
-
-console.log("en una solo linea: " + (x+y+z) + "y asi funciona" + "saludos");
-
-x = "pepito";
-y = "jaimito";
-z = "seguismundo";
-
-console.log("en una solo linea: " + (x+y+z) + "y asi funciona" + "saludos");
-
-alert(("la suma de los numeros es" + (x+y+z)));
-
-const PI = 3.1416;
-let radio = 5;
-let peri = 2 * PI * radio
-document.write("el permietro de un circulo con radio¨)
-
-
-
-// 1 ejercicio
-
-let num1 = parseInt(prompt("Introduce el primer número:"));
-let num2 = parseInt(prompt("Introduce el segundo número:"));
-
-let resta = num1 - num2;
-
-console.log("El resultado de la resta es: " + resta);
-
-document.write("El resultado de la resta es: " + resta + "<br>");
-
-
-
-// 2 ejercicio
-
-let nombre = prompt("Introduce tu nombre:");
-let localidad = prompt("Introduce la localidad donde vives:");
-let hobby = prompt("Introduce tu hobby:");
-
-console.log(`Te llamas ${nombre}, vives en ${localidad}, y te gusta ${hobby}.`);
-
-document.write(`Te llamas ${nombre}, vives en ${localidad}, y te gusta ${hobby}.`);
-
-
-
-// 3 ejercicio
-
-let radio = parseFloat(prompt("Introduce el radio del círculo:"));
-
-const PI = 3.1416;
-
-let area = PI * Math.pow(radio, 2);
-
-console.log(`El área del círculo con radio ${radio} es: ${area}`);
-
-document.write(`El área del círculo con radio ${radio} es: ${area}<br>`);
+let radio = Number(prompt( "Dime un número, lo usaremos como radio para calcular el área de un círculo."));
+const pi = 3.1416;
+let area = pi * radio * radio;
+document.writeln ( "Además, el área del círculo con el radio que proporcionaste es: " + area)
+console.log ("El área del círculo con el radio que proporcionaste es:: " + area)
